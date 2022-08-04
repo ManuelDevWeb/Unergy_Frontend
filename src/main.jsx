5a48af87c42ab0ab710a4e0d8abb132302255b0e
ManuelDev <manueldeveloper17@gmail.com>
2022-08-04 15:55:23 -0500

    feat: sass and bootstrap configuration

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+// Bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// App
 import App from "./App";
 
-import "./index.css";
+// Styles
+import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
